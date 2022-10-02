f59aee18c3ec2af1f063bcc7c36ed05b57284bfd
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-02 20:01:46 +0530

    initial Commit

--- src/Components/CovidApi.jsx
+++ src/Components/CovidApi.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './Theme.css'
+import "./Theme.css";
 
 export function CovidApi() {
   const [data, setData] = useState([]);
+  let [search, setSearch] = useState("");
+  let [filtered, setfilteredData] = useState([]);
 
   useEffect(() => {
     async function getData() {
@@ -11,22 +13,41 @@ export function CovidApi() {
         "https://data.covid19india.org/data.json"
       );
       setData(response.data.statewise);
+      setfilteredData([data, ...response.data.statewise]);
     }
     getData();
   }, []);
 
+  useEffect(() => {
+    let searched = data.filter((ser) => {
+      if (ser.state) {
+        return ser.state.includes(search.toLocaleLowerCase());
+      }
+    });
+    setfilteredData(searched);
+  }, [search]);
+
   return (
     <>
+     
       <div className="container">
-          <h1 >Statewise Covid data of India</h1>
+        <h1>Statewise Covid data of India</h1>
 
         <div>
-          <button className="active theme_button"  onClick={
-            (e)=>{
-                document.body.classList.toggle("active")
-                document.querySelector(".theme_button").classList.toggle("active")
-            }
-          }>Change Theme</button>
+          <button
+            className="active theme_button"
+            onClick={(e) => {
+              document.body.classList.toggle("active");
+              document
+                .querySelector(".theme_button")
+                .classList.toggle("active");
+                document.querySelector(".card")
+                .classList.toggle("theme_change");
+
+            }}
+          >
+            Change Theme
+          </button>
         </div>
         <div>
           <table>
@@ -40,7 +61,7 @@ export function CovidApi() {
               </tr>
             </thead>
             <tbody>
-              {data.map((item, index) => {
+              {data.map((item) => {
                 return (
                   <>
                     <tr>
@@ -48,7 +69,7 @@ export function CovidApi() {
                       <td className="td">{item.active}</td>
                       <td className="td">{item.confirmed}</td>
                       <td className="td">{item.recovered}</td>
-                      <td className="td" >{item.deaths}</td>
+                      <td className="td">{item.deaths}</td>
                     </tr>
                   </>
                 );
@@ -56,6 +77,20 @@ export function CovidApi() {
             </tbody>
           </table>
         </div>
+      </div>
+      <div className="card">
+        <input
+        className="input_field"
+        placeholder="Enter the name of State"
+          type="text"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+        {filtered.map((filter) => (
+          <p className="para1">{filter.state} Active Cases=> {filter.active} citizens</p>
+        ))}
       </div>
     </>
   );
